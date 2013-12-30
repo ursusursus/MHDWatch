aad6a66a40f20db56f0a6477c84eea33d1c78512
Ferko <Ferko@dasdsa>
2013-12-30 20:56:31 +0100

    Commit

--- src/com/ursus/mhdwatch/MainActivity.java
+++ src/com/ursus/mhdwatch/MainActivity.java
@@ -41,9 +41,10 @@ public class MainActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Hello github
 
 		init();
 		initRoutes();
